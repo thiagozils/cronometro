8e8e024a3a7e3db0244a5a6d45245deb34ace27a
Thiago Andrey Zils <thiagozils@weg.net>
2019-10-18 10:38:45 -0300

    ALTERAÇÕES EM TELAS E CONTROLLERS

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -13,19 +13,17 @@ class IndexController extends ControllerBase
         $competicao  = Competicao::findFirst(['conditions' => 'ativa = 1']);
         $this->view->competicao = $competicao ;
 
-        $query = $this->modelsManager->createQuery('SELECT DISTINCT volta.id_competidor from volta ORDER by volta.tempo asc');
+        $query = $this->modelsManager->createQuery('SELECT DISTINCT volta.id_competidor from volta ORDER by volta.tempo desc');
         $cc  = $query->execute();
 
     
         
         foreach ($cc as $compc){
-            $voltas = Volta::find(['conditions' => 'id_competidor = '.$compc->id_competidor, 'id_competicao = '.$competicao->id]);
+            $voltas = Volta::find(['conditions' => 'id_competidor = '.$compc->id_competidor.' and id_competicao = '.$competicao->id]);
             $voltaFinal[$compc->id_competidor] = $voltas;
         }
 
 
-
-        
         $this->view->voltas = $voltaFinal ;
 
 
